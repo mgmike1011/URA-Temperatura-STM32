e2f00c41eafa7eec1c12f407c8cbb5e4dbb081b1
Agnieszka_Piorko <agnpior@gmail.com>
2022-01-14 19:39:07 +0100

    Update MATLAB

--- Symulacja/okreslanie_modelu_obiektu.m
+++ Symulacja/okreslanie_modelu_obiektu.m
@@ -21,40 +21,43 @@ wejscie = amplituda_wejsciowa*ones( 1 ,liczba_probek);
 % rêcznie do kodu k, T, delay
 
 offset = Wyniki(1,2);
+ k = 16.413;
+ T = 237.67;
+ tau = 6.302;
 
 % Obiekt na podstawie dopasowania:
 % !!!! Odkomentuj, gdy wyeksportujesz dane z pidTuner !!!!
-% s = tf('s');
-% k = Plant1.Kp; %Plant1.Kp;
-% T = Plant1.Tp1;
-% delay = Plant1.Td;
-% H = k/(1+s*T)*exp(-s*delay); % model
-% disp(sprintf('Parametry modelu: k=%.2g, T=%g, delay=%g\n', k, T, delay));
+s = tf('s');
+%  %k = Plant1.Kp; %Plant1.Kp;
+%  T = Plant1.Tp1;
+%  delay = Plant1.Td;
+ H = k/(1+s*T)*exp(-s*tau); % model
+ disp(sprintf('Parametry modelu: k=%.2g, T=%g, delay=%g\n', k, T, tau));
 
 % OdpowiedŸ modelu:
 % !!!! Odkomentuj, gdy wyeksportujesz dane z pidTuner !!!!
-% odpowiedz_modelu = lsim(H,wejscie,czas);
-% odpowiedz_modelu = odpowiedz_modelu + offset; % offset - wartoœæ pierwszej temperatury
+ odpowiedz_modelu = lsim(H,wejscie,czas);
+ odpowiedz_modelu = odpowiedz_modelu + offset; % offset - wartoœæ pierwszej temperatury
  
 % B³¹d modelu:
 % !!!! Odkomentuj, gdy wyeksportujesz dane z pidTuner !!!!
-% residuum = temperatura - odpowiedz_modelu; %gdyby plik wierszowy to 
+ residuum = temperatura - odpowiedz_modelu; %gdyby plik wierszowy to 
 %odpowiedz_modelu ' (tranzpozycja)
-% suma_abs_bledow = sum(abs(residuum));
-% disp(sprintf('Suma b³êdów(abs(residuum)) = %g\n', suma_abs_bledow));
+ suma_abs_bledow = sum(abs(residuum));
+ disp(sprintf('Suma b³êdów(abs(residuum)) = %g\n', suma_abs_bledow));
 
-% figure(1);
-% plot(czas,temperatura, '.', czas, odpowiedz_modelu, '.');
-% title('Pomiary i odpowiedŸ obiektu');
-% xlabel('Czas (s)');
-% ylabel('Temperatura (C)');
-% legend('zmierzone próbki', 'odpowiedŸ uk³adu heater+BMP280');
-% axis tight;
-% 
-% figure(2);
-% plot(czas,residuum, '.');
-% title('Residuum (zmierzone próbki - odpowiedŸ uk³adu)');
-% xlabel('Czas (s)');
-% ylabel('Temperatura (C)');
-% axis tight;
-% ylim([-0.5 0.5]);
+figure(1);
+plot(czas,temperatura, '.', czas, odpowiedz_modelu, '.');
+title('Pomiary i odpowiedŸ obiektu');
+xlabel('Czas (s)');
+ylabel('Temperatura (C)');
+legend('zmierzone próbki', 'odpowiedŸ uk³adu heater+BMP280');
+axis tight;
+
+figure(2);
+plot(czas,residuum, '.');
+title('Residuum (zmierzone próbki - odpowiedŸ uk³adu)');
+xlabel('Czas (s)');
+ylabel('Temperatura (C)');
+axis tight;
+ylim([-0.5 0.5]);
--- Symulacja/weryfikacja_koncowy_wykres.m
+++ Symulacja/weryfikacja_koncowy_wykres.m
@@ -1,15 +1,16 @@
-wyniki = importdata('Wyniki_koncowe.txt'); %plik z koñcowymi zebranymi pomiarami
+%wyniki = importdata('WynikiPID.txt'); %plik z koñcowymi zebranymi pomiarami
+Wyniki = WynikiPID;
 czas = Wyniki(:,1);
 temperatura = Wyniki(:,2);
 liczba_probek = length(temperatura);
-zadana = 26; % tu ustaw nasz¹
+zadana = 27; % tu ustaw nasz¹
 
 figure(1);
 plot(czas,temperatura, 'b.');
 hold on;
 line([0 czas(end)],[zadana, zadana],'Color','red','LineStyle','--');
 hold off;
-title('Dane regulatora:, Kp=wpisz, Ki=wpisz, Kd=wpisz');
+title('Dane regulatora:, Kp=0.52, Ki=0.003, Kd=0.257');
 xlabel('Czas (s)');
 ylabel('Temperatura (C)');
 legend('Zmierzone próbki');
