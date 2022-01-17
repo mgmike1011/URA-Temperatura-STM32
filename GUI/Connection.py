74e8c522d567165021bf6b138349697719362e8b
mgmike1011 <60261042+mgmike1011@users.noreply.github.com>
2022-01-17 18:42:35 +0100

    GUI initial design

--- GUI/Connection.py
+++ GUI/Connection.py
@@ -6,8 +6,9 @@ import matplotlib as plt
 
 port = 'COM4'
 szybkosc = 115200
+
 def Connect_to_MCU():
-    hSerial = serial.Serial(port, szybkosc, timeout = 1, parity = serial.PARITY_NONE)
+    hSerial = serial.Serial(port, szybkosc, timeout = 1, parity = serial.PARITY_NONE, bytesize = serial.EIGHTBITS) # Otworzenie portu
     hSerial.reset_input_buffer()
     hSerial.flush()
 # hSerial.close() !
--- GUI/Plot.py
+++ GUI/Plot.py
@@ -7,26 +7,61 @@ from time import sleep
 
 
 def Plot_Init():
+    # Wartości początkowe
+    # "Temp":wartosc
+    temperature = 0 # Aktualna temperatura
+    temperature_all = [] # Wektor temperatur
+    # "t":wartosc
+    t_all = [] # Wektor czasu
+    t = 0 # Czas
+    t_prev = 0 # Czas poprzedni
+    # "Temp_set":wartosc
+    temperature_set_all = [] # Wektor zadanych temperatur
+    temperature_set = 0 # Aktulana temperatura zadana
+    # "Kp":wartosc
+    Kp = 0
+    # "Ki":wartosc
+    Ki = 0
+    # "Kd":wartosc
+    Kd = 0
+    # "u":wartosc
+    u = 0 # Sterowanie aktualne
+    u_all = [] # Wektor sterowania
     plt.ion()
     Connect_to_MCU()
 
-temperature_all = []
-t_all = []
-temperature = 0
-t = 0
+
 
 def get_data():
     tmp = hSerial.readline()
     try:
+        # Format ramki JSON:
+        # {"Temp":wartosc,"t":wartosc,"Temp_set":wartosc,"Kp":wartosc,"Ki":wartosc,"Kd":wartosc,"u":wartosc}
         sample = json.loads(tmp)
-        temperature = sample["Temp"] #Odczytanie temperatury
-        temperature_all.append(temperature) #Dodanie do całego zbioru
-        t = sample["t"] #Odczytanie temperatury
-        t_all.append(t) #Dodanie do całego zbioru
+        # "Temp":wartosc
+        temperature = sample["Temp"] # Odczytanie temperatury aktualnej
+        temperature_all.append(temperature) # Dodanie do całego zbioru
+        # "t":wartosc
+        t = sample["t"] # Czas
+        t_prev = t_prev + t
+        t_all.append(t_prev)
+        # "Temp_set":wartosc 
+        temperature_set = sample["Temp_set"] # Temperatura zadana
+        temperature_set_all.append(temperature_set)
+        # "Kp":wartosc
+        Kp = sample["Kp"]
+        # "Ki":wartosc
+        Ki = sample["Ki"]
+        # "Kd":wartosc
+        Kd = sample["Kd"]
+        # "u":wartosc
+        u = sample["u"] # Sygnał sterujący
+        u_all.append(u)
     except ValueError:
         print("JSON Problem")
         hSerial.flush()
         hSerial.reset_input_buffer()
+
 def show_plot():
     get_data()
     plt.title("URA - Przebieg temperatury")
